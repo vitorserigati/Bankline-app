abca2b3ccffff3f65d20c7b4b54cf79e9596c2be
Vitor Serigati <vitor_serigati@hotmail.com>
2022-05-08 18:15:08 -0300

    Páginas prontas, inciando relação com o bd

---
+++ src/app/components/movimentacao-new/movimentacao-new.component.ts
@@ -0,0 +1,35 @@
+// compoenents/movimentacao-new.component.ts
+
+import { Component, OnInit } from '@angular/core';
+import { CorrentistaService } from 'src/app/services/correntista.service';
+import { MovimentacaoService } from 'src/app/services/movimentacao.service';
+
+@Component({
+  selector: 'app-movimentacao-new',
+  templateUrl: './movimentacao-new.component.html',
+  styleUrls: ['./movimentacao-new.component.css']
+})
+export class MovimentacaoNewComponent implements OnInit {
+  correntistas:any;
+
+  constructor(
+    private movimentacaoService: MovimentacaoService,
+    private correntistaService: CorrentistaService,
+    ) { }
+
+  ngOnInit(): void {
+    this.exibirCorrentistas();
+  }
+  exibirCorrentistas(): void {
+    this.correntistaService.list()
+      .subscribe(
+        data => {
+          this.correntistas = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+}
